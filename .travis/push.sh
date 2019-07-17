2e20f8d4ab3390c124ead89310d2407c03504a92
sebastienHuertas <contact@sebastien-huertas.fr>
2019-07-17 15:34:53 +0200

    :white_check_mark: deploy test

--- .travis/push.sh
+++ .travis/push.sh
@@ -1,16 +1,10 @@
 #!/bin/sh
 
-setup_git(){
-    git config --global user.email "$GIT_USER_MAIL"
-    git config --global user.name "$GIT_USER_NAME"
-}
+-n $DEPLOY_HOST $DEPLOY_PORT << END_SCRIPT
+quote USER $DEPLOY_LOGIN
+quote PASS $DEPLOY_PASSWORD
 
-merge_files(){
-    git checkout $BRANCH_TARGET
-    git merge $CURRENT_BRANCH
-    git commit -m 'Branch merge by $GIT_USER_NAME'
-    git push origin master
-}
-
-echo $GIT_USER_MAIL 
-echo $GIT_USER_NAME 
+put $TRAVIS_BUILD_DIR/dist
+quit
+END_SCRIPT
+exit 0
